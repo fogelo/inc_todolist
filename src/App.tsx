c3c8c8ced8ca9c5ff95ac4739497a85221c490b4
antonorlov <fogelo@yandex.ru>
2022-02-23 16:27:21 +0300

    add the ability to add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {v1} from 'uuid';
+
+type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
 
 function App() {
-  return (
-    <div className="App">
-        hello
-    </div>
-  );
+    let [tasks, setTasks] = useState<Array<TaskType>>([])
+    let [inputValue, setInputValue] = useState('')
+
+    function addTask() {
+        const newTask = {id: v1(), title: inputValue, isDone: false}
+        setTasks([...tasks, newTask])
+        setInputValue('')
+    }
+
+    return (
+        <div className="App">
+            <div>What to learn</div>
+            <input value={inputValue} onChange={(e) => setInputValue(e.currentTarget.value)} type="text"/>
+            <button onClick={addTask}>+</button>
+
+            <ul>
+                {tasks.map(item => <li key={item.id}>{item.title}</li>)}
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
