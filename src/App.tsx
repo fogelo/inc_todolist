cfe5251d8f554026d220c727cba146c3c28ad5b5
antonorlov <fogelo@yandex.ru>
2022-02-23 18:37:10 +0300

    add the ability to remove task and change checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,27 @@ function App() {
     let [inputValue, setInputValue] = useState('')
 
     function addTask() {
-        const newTask = {id: v1(), title: inputValue, isDone: false}
-        setTasks([...tasks, newTask])
-        setInputValue('')
+        if (inputValue) {
+            const newTask = {id: v1(), title: inputValue, isDone: false}
+            setTasks([...tasks, newTask])
+            setInputValue('')
+        } else {
+            return
+        }
+
+    }
+
+    function removeTask(id: string) {
+        setTasks(tasks.filter(item => item.id !== id))
+    }
+
+    function changeCheckbox(id: string) {
+        let task = tasks.find(item => item.id === id)
+        if(task){
+            task.isDone = !task.isDone
+            setTasks([...tasks])
+        }
+
     }
 
     return (
@@ -26,7 +44,11 @@ function App() {
             <button onClick={addTask}>+</button>
 
             <ul>
-                {tasks.map(item => <li key={item.id}>{item.title}</li>)}
+                {tasks.map(item => <li key={item.id}>
+                    <input onClick={() => changeCheckbox(item.id)} checked={item.isDone} type="checkbox"/>
+                    {item.title}
+                    <button onClick={() => removeTask(item.id)}>-</button>
+                </li>)}
             </ul>
 
         </div>
